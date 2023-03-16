c3cd335787494c650ed886837a69d28574110233
jp022 <j.parda.lara@iescristobaldemonroy.es>
2023-03-16 07:56:58 +0100

    Segundo commit de la rama cambios

--- hola-mundo/src/pruebas/ClaseNueva.java
+++ hola-mundo/src/pruebas/ClaseNueva.java
@@ -4,6 +4,7 @@ public class ClaseNueva {
 
 	public static void main(String[] args) {
 		System.out.println("Esta es la clase nueva con una linea.");
+		System.out.println("Esto es para el segundo commit.");
 
 	}
 
