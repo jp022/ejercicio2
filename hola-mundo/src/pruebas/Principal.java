d23ac654d69c46ce7ffd0b673a1cde1ebdea93f4
jp022 <j.parda.lara@iescristobaldemonroy.es>
2023-03-16 07:48:27 +0100

    Segundo commit

--- hola-mundo/src/pruebas/Principal.java
+++ hola-mundo/src/pruebas/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Esto es un cambio para poder hacer el commit.");
+		System.out.println("Esto es para hacer el segundo commit.");
 		System.out.println("Adios.");	
 		
 	}
