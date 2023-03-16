6ba45de453e14b73c3593fefdff0b4efa6ceb870
jp022 <j.parda.lara@iescristobaldemonroy.es>
2023-03-16 07:58:56 +0100

    Cuarto commit de la rama cambios.

--- hola-mundo/src/pruebas/SegundaClaseNueva.java
+++ hola-mundo/src/pruebas/SegundaClaseNueva.java
@@ -4,6 +4,7 @@ public class SegundaClaseNueva {
 
 	public static void main(String[] args) {
 		System.out.println("Esta es la segunda clase nueva.");
+		System.out.println("Esto es para el cuarto commit.");
 	}
 
 }
