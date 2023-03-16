ac8058c3bf6f9152d127885ecfe0add230cd3281
jp022 <j.parda.lara@iescristobaldemonroy.es>
2023-03-16 07:46:43 +0100

    Primer commit

---
+++ hola-mundo/src/pruebas/Principal.java
@@ -0,0 +1,12 @@
+package pruebas;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+		System.out.println("Esto es un cambio para poder hacer el commit.");
+		System.out.println("Adios.");	
+		
+	}
+
+}
